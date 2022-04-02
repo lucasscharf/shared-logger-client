a47471b781555066bad207471be9309df4d6b9df
João Lucas Scharf <lucas.scharf@gmail.com>
2022-04-02 13:10:06 -0300

    rm log error

--- initialization-script.sh
+++ initialization-script.sh
@@ -1,3 +1,9 @@
 git clone https://github.com/lucasscharf/shared-logger-client
-cd ~/shared-logger-client/server/
 cd ~/shared-logger-client/URingPaxos/
+mvn clean install
+
+cd ~/shared-logger-client/URingPaxos/clients/SMR 
+mvn clean install
+
+cd ~/shared-logger-client/server/
+mvn clean install
