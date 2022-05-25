ad9a774a8eae581312dec58e6e02f5d90f5c4006
João Lucas Scharf <lucas.scharf@gmail.com>
2022-05-25 18:47:27 -0300

    change create replica

--- server/src/main/java/org/acme/LoggerController.java
+++ server/src/main/java/org/acme/LoggerController.java
@@ -82,12 +82,12 @@ public class LoggerController {
     @Path("registerReplica")
     public Response registerReplica(LoggerConfig config) throws Exception {
         logger.info("Registering replica logger");
-        // String[] args = { config.ring + "," + config.id + ",0", "0", config.url };
-        // Replica.main(args);
-        // replicaLoggerClient = new Replica("0", config.ring,config.id,0,config.url);
-        replicaLoggerClient = new ReplicaLoggerClient("0", config.ring,config.id,0, zookeeperUrl);
-        logger.info("Replica logger [{}]", replicaLoggerClient);
-        replicaLoggerClient.start();
+        String[] args = { config.ring + "," + config.id + ",0", "0", zookeeperUrl };
+        Replica.main(args);
+        // replicaLoggerClient = new Replica("0", config.ring,config.id,0,zookeeperUrl);
+        // replicaLoggerClient = new ReplicaLoggerClient("0", config.ring,config.id,0, zookeeperUrl);
+        // logger.info("Replica logger [{}]", replicaLoggerClient);
+        // replicaLoggerClient.start();
         return Response.ok().build();
     }
 
