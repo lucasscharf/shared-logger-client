60a57fe75aa7fa69a8c66002ddef0c106640de21
João Lucas Scharf <lucas.scharf@gmail.com>
2022-11-30 10:43:07 -0300

    add sleep inside clean zookeeper

--- scripts/clean_zookeeper.sh
+++ scripts/clean_zookeeper.sh
@@ -5,6 +5,8 @@ docker rm zookeeper
 cd ~/shared-logger-client
 docker-compose up -d zookeeper
 
+sleep 3
+
 docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos 0
 docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology1 0
 docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology2 0
