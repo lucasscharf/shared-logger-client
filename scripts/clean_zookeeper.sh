6af6021302255914f1fffba4296b6e57dec6d340
João Lucas Scharf <lucas.scharf@gmail.com>
2022-11-30 10:39:03 -0300

    add script to clean zookeeper after execution

---
+++ scripts/clean_zookeeper.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+docker stop zookeeper
+docker rm zookeeper
+cd ~/shared-logger-client
+docker-compose up -d zookeeper
+
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology1 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology2 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology1/config 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology2/config 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology1/config/learner_recovery 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh create /ringpaxos/topology2/config/learner_recovery 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh set /ringpaxos/topology1/config/learner_recovery 0
+docker exec zookeeper /opt/zookeeper/bin/zkCli.sh set /ringpaxos/topology2/config/learner_recovery 0
