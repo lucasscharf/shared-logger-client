aac01e627f9273f68ac7aa7ec68bbb341d08a28e
João Lucas Scharf <lucas.scharf@gmail.com>
2022-11-30 10:04:11 -0300

    first version of automation script

--- scripts/executar_tudo.sh
+++ scripts/executar_tudo.sh
@@ -12,17 +12,25 @@ ipClient=pc795.emulab.net
 outputFile=~/code/shared-logger-client/evaluation/thinking_time_50/io_sem_4_500_001/
 
 threads=(2 4 8 16 32 64 128 256 512 1024)
+apps=(io cpu)
+logs=(sem cou dec)
 ips=($ipProposer $ipAcceptor $ipLogger1 $ipLogger2 $ipReplica1 $ipReplica2 $ipReplica3)
 
-ssh lucas123@ipProposer ~/shared-logger-client/scripts/run_proposer.sh
 
-for thread in "${threads[@]}" 
-do
-	echo "$thread"
-done 
+ssh lucas123@$ipProposer '~/shared-logger-client/scripts/run_proposer.sh &' &
+ssh lucas123@$ipAcceptor '~/shared-logger-client/scripts/run_acceptor.sh &' &
+ssh lucas123@$ipReplica1 "~/shared-logger-client/scripts/run_cpu_sem_replica_1.sh &" &
+ssh lucas123@$ipReplica2 "~/shared-logger-client/scripts/run_cpu_sem_replica_2.sh &" &
+ssh lucas123@$ipReplica3 "~/shared-logger-client/scripts/run_cpu_sem_replica_3.sh &" &
+
+sleep 3
+
+ssh lucas123@$ipClient "~/shared-logger-client/scripts/run_experiment.sh"
+
+./pegarLogs $ipLogger1 $ipLogger2 $ipReplica1 $ipReplica2 $ipReplica3 $ipClient $outputFile
 
 for ip in "${ips[@]}" 
 do 
-	echo $ip
+	ssh lucas123@$ipProposer '~/shared-logger-client/scripts/kill_all_java.sh &' &
 done 
-#./pegarLogs $ipLogger1 $ipLogger2 $ipReplica1 $ipReplica2 $ipReplica3 $ipClient $outputFile
+
--- scripts/run_proposer.sh
+++ scripts/run_proposer.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-killall -9 java
-
 cd ~/shared-logger-client/URingPaxos/target/Paxos-trunk ; ./thriftnode.sh 1 1:PA 10.10.1.1
