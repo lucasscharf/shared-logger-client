08350e2015f31bf581f28b59d4483a585fb80a69
João Lucas Scharf <lucas.scharf@gmail.com>
2022-12-07 22:13:07 -0300

    trying add flush

--- URingPaxos/clients/SMR/src/java/ch/usi/da/smr/Replica.java
+++ URingPaxos/clients/SMR/src/java/ch/usi/da/smr/Replica.java
@@ -32,6 +32,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -39,6 +40,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
@@ -254,9 +256,14 @@ public class Replica implements Receiver {
 				try {
 					if (embebedLog) {
 						String stringToSave = command.toString() + "\n";
-						Files.write(path,
-								stringToSave.getBytes(),
-								StandardOpenOption.APPEND, StandardOpenOption.SYNC);
+						// Files.write(path,
+						// 		stringToSave.getBytes(),
+						// 		StandardOpenOption.APPEND, StandardOpenOption.SYNC);
+							File file = path.toFile();
+							FileWriter writer = new FileWriter(file, true);
+							writer.write(Arrays.toString(command.getValue()));
+							writer.flush();
+							writer.close();
 					}
 				} catch (IOException e) {
 					logger.error("", e);
@@ -268,7 +275,13 @@ public class Replica implements Receiver {
 						if (useDiskDb) {
 							Path fileToSave = fileDatabase.resolve(command.getKey());
 							try {
-								Files.write(fileToSave, command.getValue(), StandardOpenOption.APPEND, StandardOpenOption.SYNC);
+								File file = fileToSave.toFile();
+								file.createNewFile();
+								FileWriter writer = new FileWriter(file, true);
+								writer.write(Arrays.toString(command.getValue()));
+								writer.flush();
+								writer.close();
+								// Files.write(fileToSave, command.getValue(), StandardOpenOption.APPEND, StandardOpenOption.SYNC);
 							} catch (Exception ex) {
 								ex.printStackTrace();
 							}
