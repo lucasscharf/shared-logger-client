c81087d54c67188add90d92f48a56c079ff2b989
João Lucas Scharf <lucas.scharf@gmail.com>
2022-12-10 18:38:52 -0300

    change scripts to use /media/disk1 instead /tmp

--- scripts/run_cpu_dec_replica_3.sh
+++ scripts/run_cpu_dec_replica_3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/shared-logger-client/URingPaxos/clients/SMR/target/SMR-trunk ;  ./replica.sh 1:L,15,0 0 10.10.1.1 false /tmp/ false > /tmp/execucao
+cd ~/shared-logger-client/URingPaxos/clients/SMR/target/SMR-trunk ;  ./replica.sh 1:L,15,0 0 10.10.1.1 false /media/disk1/ false > /tmp/execucao
--- scripts/run_quarkus_createloggers.sh
+++ scripts/run_quarkus_createloggers.sh
@@ -6,4 +6,4 @@ export nodes_replicas_url=http://node8:8888,http://node7:8888
 java -jar ~/shared-logger-client/server/target/shared-logger-1.0.0-SNAPSHOT-runner.jar > /tmp/execucao &
 
 sleep 5
-curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":4,\"pathPrefix\":\"/tmp/\",\"replicas\":1,\"ring\":\"1\"}"
+curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":4,\"pathPrefix\":\"/media/disk1/\",\"replicas\":1,\"ring\":\"1\"}"
