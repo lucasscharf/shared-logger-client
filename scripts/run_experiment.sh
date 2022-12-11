bc70ed6265d670d16e24207a09c26e1aaf3b67fb
Joao Lucas Scharf <lucas123@localhost>
2022-12-10 22:11:44 -0700

    reduce the number of latencies collected

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -3,4 +3,4 @@
 number_of_threads=$1
 
 cd ~/shared-logger-client/scripts
-./execute_experiment.sh $number_of_threads 90 1024 10.10.1.1 50 > /tmp/execucao 
+./execute_experiment.sh $number_of_threads 30 1024 10.10.1.1 50 100 1000 > /tmp/execucao 
