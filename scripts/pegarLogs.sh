2f46e6cfe36eb20360f68aa3369bee0d1240ae2e
João Lucas Scharf <lucas.scharf@gmail.com>
2022-12-20 18:30:08 -0300

    fix some reading script

--- scripts/pegarLogs.sh
+++ scripts/pegarLogs.sh
@@ -19,11 +19,11 @@ latencyPath=$(cat /tmp/client.log | grep "Raw path" | cut -d'[' -f3  | cut -d']'
 scp lucas123@$ipClient:$latencyPath $outputFile/client_latency.csv
 
 scp lucas123@$ipClient:/tmp/execucao_2 /tmp/client_2.log
-latencyPath=$(cat /tmp/client.log | grep "Raw path" | cut -d'[' -f3  | cut -d']' -f1 | cut -d' ' -f2 )
+latencyPath=$(cat /tmp/client_2.log | grep "Raw path" | cut -d'[' -f3  | cut -d']' -f1 | cut -d' ' -f2 )
 scp lucas123@$ipClient:$latencyPath $outputFile/client_latency_2.csv
 
 scp lucas123@$ipClient:/tmp/execucao $outputFile/client.log
-scp lucas123@$ipLogger1:/tmp/execucao $outputFile/logger_1.csv
+#scp lucas123@$ipLogger1:/tmp/execucao $outputFile/logger_1.csv
 scp lucas123@$ipLogger2:/tmp/execucao $outputFile/logger_2.csv
 
 scp lucas123@$ipReplica1:/tmp/execucao $outputFile/replica_1.csv
