f42763318dfe6c988509e59260b40a05fa0f73bc
Joao Lucas Scharf <lucas123@localhost>
2022-12-21 07:21:48 -0700

    add more readings

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -3,6 +3,6 @@
 number_of_threads=$1
 
 cd ~/shared-logger-client/scripts
-./execute_experiment.sh $number_of_threads 30 1024 10.10.1.1 50 100 2000 > /tmp/execucao &
-./execute_experiment_ring_2.sh $number_of_threads 30 1024 10.10.1.1 50 100 2000 > /tmp/execucao_2 
+./execute_experiment.sh $number_of_threads 30 1024 10.10.1.1 50 100 8000 > /tmp/execucao &
+./execute_experiment_ring_2.sh $number_of_threads 30 1024 10.10.1.1 50 100 8000 > /tmp/execucao_2 
 exit
