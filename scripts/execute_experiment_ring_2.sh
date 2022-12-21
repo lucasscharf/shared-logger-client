1d8beacbc57beb244f39464a5da33500909fcf14
João Lucas Scharf <lucas.scharf@gmail.com>
2022-12-20 22:53:43 -0300

    fix latency readings for few threads

--- scripts/execute_experiment_ring_2.sh
+++ scripts/execute_experiment_ring_2.sh
@@ -7,4 +7,4 @@ thinkingTime=$5
 writePercentage=$6
 commandTrackerNumber=$7
 
-printf "start ${max_threads_per_node} ${requests_per_thread} ${thread_size} \n" | ~/shared-logger-client/URingPaxos/clients/SMR/target/SMR-trunk/client.sh 2,21 ${ip} ${thinkingTime} ${writePercentage} 
+printf "start ${max_threads_per_node} ${requests_per_thread} ${thread_size} \n" | ~/shared-logger-client/URingPaxos/clients/SMR/target/SMR-trunk/client.sh 2,21 ${ip} ${thinkingTime} ${writePercentage} ${commandTrackerNumber}
