4b55dba6699b75f6e3c1b46973c6bc4986ae66fd
João Lucas Scharf <lucas.scharf@gmail.com>
2022-12-26 22:31:12 -0300

    improvements to get logger latency

--- scripts/run_quarkus_createloggers.sh
+++ scripts/run_quarkus_createloggers.sh
@@ -6,5 +6,5 @@ export nodes_replicas_url=http://node8:8888,http://node7:8888
 java -jar ~/shared-logger-client/server/target/shared-logger-1.0.0-SNAPSHOT-runner.jar > /tmp/execucao &
 
 sleep 5
-curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":4,\"pathPrefix\":\"/media/disk1/\",\"replicas\":1,\"ring\":\"1\"}"
-curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":5,\"pathPrefix\":\"/media/disk1/\",\"replicas\":1,\"ring\":\"2\"}"
+curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":4,\"pathPrefix\":\"/media/disk1/\",\"replicas\":1,\"ring\":\"1\",\"trackerNumber\":1000}"
+curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":5,\"pathPrefix\":\"/media/disk1/\",\"replicas\":1,\"ring\":\"2\",\"trackerNumber\":1000}"
