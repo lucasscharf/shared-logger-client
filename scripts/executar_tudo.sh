9dad827cc4e2794a5c13ede9ec9962bb329f4c51
João Lucas Scharf <lucas.scharf@gmail.com>
2022-12-27 22:30:13 -0300

    finally add script that runs everything

--- scripts/executar_tudo.sh
+++ scripts/executar_tudo.sh
@@ -1,56 +1,97 @@
+
 #!/bin/bash
 
-ipZookeeper=pc822.emulab.net
-ipProposer=pc731.emulab.net
-ipAcceptor=pc725.emulab.net
-ipReplica1=pc738.emulab.net
-ipReplica2=pc740.emulab.net
-ipReplica3=pc831.emulab.net
-ipClient=pc726.emulab.net
-ipLogger1=pc825.emulab.net
-ipLogger2=pc829.emulab.net
+ipZookeeper=pc844.emulab.net
+ipProposer=pc711.emulab.net
+ipAcceptor=pc848.emulab.net
+ipReplica1=pc706.emulab.net
+ipReplica2=pc708.emulab.net
+ipReplica3=pc709.emulab.net
+ipProposer_ring_2=pc845.emulab.net
+ipAcceptor_ring_2=pc842.emulab.net
+ipReplica1_ring_2=pc858.emulab.net
+ipReplica2_ring_2=pc704.emulab.net
+ipReplica3_ring_2=pc841.emulab.net
+ipClient=pc847.emulab.net
+ipLogger1=pc853.emulab.net
+ipLogger2=pc702.emulab.net
 
-threads=(2 4 8 16 32 64 128 256 512 1024)
 apps=(io cpu)
 logs=(sem cou dec)
-ips=($ipReplica1 $ipLogger1 $ipProposer $ipAcceptor $ipLogger2 $ipReplica2 $ipReplica3)
+threads=(2 4 8 16 32 64 128 256 512 1024 2048)
 
-for log in "${logs[@]}"
+ips=($ipReplica1 $ipLogger1 $ipProposer $ipAcceptor $ipLogger2 $ipReplica2 $ipReplica3 $ipReplica1 $ipLogger1_ring_2 $ipProposer_ring_2 $ipAcceptor_ring_2 $ipReplica1_ring_2 $ipReplica2_ring_2 $ipReplica3_ring_2 $ipClient)
+
+cd ~/shared-logger-client/scripts 
+
+for ip in "${ips[@]}" 
 do
-	for app in "${apps[@]}" 
+	ssh lucas123@$ip 'sudo chown lucas123 /media ; sudo rm -rf /media/disk1 ; sudo chmod 777 /media;  mkdir /media/disk1'
+done 
+
+for ip in "${ips[@]}" 
+do 
+	ssh lucas123@$ip '~/shared-logger-client/scripts/kill_all_java.sh'
+done 
+
+for app in "${apps[@]}" 
+do
+	for log in "${logs[@]}"
 	do
 		for thread in "${threads[@]}"
 		do
-			outputFile=~/code/shared-logger-client/evaluation/thinking_time_50/$app\_$log\_$thread\_90_001/
+			if [ $log = dec ]; then
+				ssh lucas123@$ipLogger2 "~/shared-logger-client/scripts/run_quarkus_createloggers.sh" &
+				sleep 6
+			fi
+
+			outputFile=~/shared-logger-client/evaluation/thinking_time_50/1ring/$app\_$log\_$thread\_90_001/
+			# outputFile=~/shared-logger-client/evaluation/thinking_time_50/2rings/$app\_$log\_$thread\_90_001/
+
 			ssh lucas123@$ipProposer '~/shared-logger-client/scripts/run_proposer.sh' & 
 			ssh lucas123@$ipAcceptor '~/shared-logger-client/scripts/run_acceptor.sh' & 
 			ssh lucas123@$ipReplica1 "~/shared-logger-client/scripts/run_$app\_$log\_replica_1.sh" &
 			ssh lucas123@$ipReplica2 "~/shared-logger-client/scripts/run_$app\_$log\_replica_2.sh" &
 			ssh lucas123@$ipReplica3 "~/shared-logger-client/scripts/run_$app\_$log\_replica_3.sh" &
+			ssh lucas123@$ipProposer_ring_2 '~/shared-logger-client/scripts/run_proposer_ring_2.sh' & 
+			ssh lucas123@$ipAcceptor_ring_2 '~/shared-logger-client/scripts/run_acceptor_ring_2.sh' & 
+			ssh lucas123@$ipReplica1_ring_2 "~/shared-logger-client/scripts/run_$app\_$log\_replica_1_ring_2.sh" &
+			ssh lucas123@$ipReplica2_ring_2 "~/shared-logger-client/scripts/run_$app\_$log\_replica_2_ring_2.sh" &
+			ssh lucas123@$ipReplica3_ring_2 "~/shared-logger-client/scripts/run_$app\_$log\_replica_3_ring_2.sh" &
 			
-			if [ $log = dec ]; then
-				ssh lucas123@$ipLogger1 "~/shared-logger-client/scripts/run_quarkus_simple.sh" &
-				ssh lucas123@$ipLogger2 "~/shared-logger-client/scripts/run_quarkus_createloggers.sh" &
-			fi
 			sleep 5
 
+			echo "Executando o experimento com $app $log $thread "
 			ssh lucas123@$ipClient "~/shared-logger-client/scripts/run_experiment.sh $thread"
+			echo "Killing sshs"
+			killall -9 ssh
+
+			if [ $log = dec ]; then
+				ssh lucas123@$ipLogger2 "curl -XPOST localhost:8888/closeLoggers" 
+			fi
 
 			for ip in "${ips[@]}" 
 			do 
-				ssh lucas123@$ip '~/shared-logger-client/scripts/kill_all_java.sh' 
+				ssh lucas123@$ip '~/shared-logger-client/scripts/kill_all_java.sh'
 			done 
 
-			./pegarLogs.sh $ipLogger1 $ipLogger2 $ipReplica1 $ipReplica2 $ipReplica3 $ipClient $outputFile
+			echo "Getting logs"
+			~/shared-logger-client/scripts/pegarLogs.sh $ipLogger1 $ipLogger2 $ipReplica1 $ipReplica2 $ipReplica3 $ipClient $outputFile $ipReplica1_ring_2 $ipReplica2_ring_2 $ipReplica3_ring_2
 
+			echo "Cleaning tmps"
 			for ip in "${ips[@]}" 
 			do 
-				ssh lucas123@$ip '~/shared-logger-client/scripts/clean_tmp.sh' & 
+				echo "Cleaning ip: $ip"
+				ssh lucas123@$ip '~/shared-logger-client/scripts/clean_tmp.sh'
 			done 
-			sleep 5
 
-			ssh lucas123@$ipZookeeper '~/shared-logger-client/scripts/clean_zookeeper.sh >> /dev/null'
+			echo "Rebuilding zookeeper"
+			~/shared-logger-client/scripts/clean_zookeeper.sh > /dev/null
 			killall -9 ssh
 		done
 	done
 done
+
+echo "Fim de execução"
+
+
