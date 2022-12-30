ac7f6a40035792842a112f4b3d7d74d0798340e2
Joao Lucas Scharf <lucas123@localhost>
2022-12-29 18:14:56 -0700

    add readings for logger

--- scripts/run_quarkus_stressTest.sh
+++ scripts/run_quarkus_stressTest.sh
@@ -9,4 +9,6 @@ export nodes_replicas_url=http://node8:8888,http://node7:8888
 java -jar ~/shared-logger-client/server/target/shared-logger-1.0.0-SNAPSHOT-runner.jar > /tmp/execucao &
 
 sleep 5
-curl -X POST "http://localhost:8888/$threadCount/$ringCount"
+curl -X POST http://localhost:8888/executeLoggerPerformanceTest/$threadCount/$ringCount
+
+killall -9 java
