35c1c23dc5728f86aa2d3dc7751b035784cb271e
João Lucas Scharf <lucas.scharf@gmail.com>
2023-04-02 22:13:31 -0300

    improving logs and removing error in proposer 3

--- scripts/recovery_logs_4rings.sh
+++ scripts/recovery_logs_4rings.sh
@@ -11,9 +11,13 @@ outputFile=$8
 
 mkdir $outputFile
 
-scp lucas123@$sun:/tmp/execucao /tmp/client.log
+echo "Sun: $sun"
+echo "Uranus: $uranus"
+echo "Jupyter: $jupyter"
+
+scp lucas123@$sun:/tmp/execucao_1 /tmp/client.log
 latencyPath=$(cat /tmp/client.log | grep "Raw path" | cut -d'[' -f3  | cut -d']' -f1 | cut -d' ' -f2 )
-scp lucas123@$sun:$latencyPath $outputFile/client_latency.csv
+scp lucas123@$sun:$latencyPath $outputFile/client_latency_1.csv
 
 scp lucas123@$sun:/tmp/execucao_2 /tmp/client_ring_2.log
 latencyPath=$(cat /tmp/client_ring_2.log | grep "Raw path" | cut -d'[' -f3  | cut -d']' -f1 | cut -d' ' -f2 )
@@ -27,27 +31,16 @@ scp lucas123@$sun:/tmp/execucao_4 /tmp/client_ring_4.log
 latencyPath=$(cat /tmp/client_ring_4.log | grep "Raw path" | cut -d'[' -f3  | cut -d']' -f1 | cut -d' ' -f2 )
 scp lucas123@$sun:$latencyPath $outputFile/client_latency_4.csv
 
-scp lucas123@$sun:/tmp/execucao $outputFile/client.log
+scp lucas123@$sun:/tmp/execucao_1 $outputFile/client_ring_1.log
 scp lucas123@$sun:/tmp/execucao_2 $outputFile/client_ring_2.log
 scp lucas123@$sun:/tmp/execucao_3 $outputFile/client_ring_3.log
 scp lucas123@$sun:/tmp/execucao_4 $outputFile/client_ring_4.log
 
-scp lucas123@$uranus:/tmp/execucao $outputFile/logger_2.csv
-scp lucas123@$uranus:/tmp/loggerStats $outputFile/logger_latency.csv
+#scp lucas123@$uranus:/tmp/execucao $outputFile/logger_2.csv
+#scp lucas123@$uranus:/tmp/loggerStats $outputFile/logger_latency.csv
 
 scp lucas123@$jupyter:/tmp/execucao_1 $outputFile/replica_1_ring_1.csv
 scp lucas123@$jupyter:/tmp/execucao_2 $outputFile/replica_1_ring_2.csv
 scp lucas123@$jupyter:/tmp/execucao_3 $outputFile/replica_1_ring_3.csv
 scp lucas123@$jupyter:/tmp/execucao_4 $outputFile/replica_1_ring_4.csv
 
-#scp lucas123@$ipLogger1:/tmp/execucao $outputFile/logger_1.csv
-#scp lucas123@$ipLogger1:/tmp/loggerStats $outputFile/logger_latency.csv
-scp lucas123@$ipLogger2:/tmp/loggerStats $outputFile/logger_latency.csv
-
-scp lucas123@$ipReplica1:/tmp/execucao $outputFile/replica_1.csv
-scp lucas123@$ipReplica2:/tmp/execucao $outputFile/replica_2.csv
-scp lucas123@$ipReplica3:/tmp/execucao $outputFile/replica_3.csv
-
-scp lucas123@$ipReplica1_ring_2:/tmp/execucao $outputFile/replica_1_ring_2.csv
-scp lucas123@$ipReplica2_ring_2:/tmp/execucao $outputFile/replica_2_ring_2.csv
-scp lucas123@$ipReplica3_ring_2:/tmp/execucao $outputFile/replica_3_ring_2.csv
--- scripts/run_proposer_ring_3.sh
+++ scripts/run_proposer_ring_3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/shared-logger-client/URingPaxos/target/Paxos-trunk/thriftnode.sh 31 3:PA 10.10.1.1 > > /tmp/execucao_proposer_3
+~/shared-logger-client/URingPaxos/target/Paxos-trunk/thriftnode.sh 31 3:PA 10.10.1.1 > /tmp/execucao_proposer_3
