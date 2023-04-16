db8d827cd2209409c74807aec2112fe3ce502ede
João Lucas Scharf <lucas.scharf@gmail.com>
2023-04-16 14:29:47 -0300

    fix script to register logger with multiples disks

--- scripts/run_quarkus_createloggers.sh
+++ scripts/run_quarkus_createloggers.sh
@@ -7,6 +7,6 @@ java -jar ~/shared-logger-client/server/target/shared-logger-1.0.0-SNAPSHOT-runn
 
 sleep 5
 curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":4,\"pathPrefix\":\"/media/disk1/\",\"replicas\":2,\"ring\":\"1\",\"trackerNumber\":1000}"
-curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":5,\"pathPrefix\":\"/media/disk1/\",\"replicas\":2,\"ring\":\"2\",\"trackerNumber\":1000}"
-curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":6,\"pathPrefix\":\"/media/disk1/\",\"replicas\":2,\"ring\":\"3\",\"trackerNumber\":1000}"
-curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":7,\"pathPrefix\":\"/media/disk1/\",\"replicas\":2,\"ring\":\"4\",\"trackerNumber\":1000}"
+curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":5,\"pathPrefix\":\"/media/disk2/\",\"replicas\":2,\"ring\":\"2\",\"trackerNumber\":1000}"
+curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":6,\"pathPrefix\":\"/media/disk3/\",\"replicas\":2,\"ring\":\"3\",\"trackerNumber\":1000}"
+curl -X POST "http://localhost:8888/register" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"id\":7,\"pathPrefix\":\"/media/disk4/\",\"replicas\":2,\"ring\":\"4\",\"trackerNumber\":1000}"
